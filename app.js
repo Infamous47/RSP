e7a95316675ef49d0312daa8d358106652d8eed1
Infamous47 <ruprejahb@gmail.com>
2023-07-04 22:43:10 +0530

    Complete project

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+function playerSelection() {
+    let x = prompt("Please enter your choice");
+    x = x.toLowerCase();
+    return x;
+}
 
 function compSelection() {
     const choice = ["rock", "paper", "scissor"];
@@ -5,55 +10,59 @@ function compSelection() {
     return choice[x]
 }
 
-const playerChoice = playerSelection();
-const compChoice = compSelection();
+let playerChoice;
+let compChoice;
 
-let playerScore = 1;
-let compScore = 1;
-let i = 0;
+let playerScore = 0;
+let compScore = 0;
 
-
-for (; i < 5; i++) {
+while (playerScore < 5 && compScore < 5) {
+    playerChoice = playerSelection();
+    compChoice = compSelection();
     round();
-    playerSelection();
-}
-function playerSelection() {
-    let x = prompt("Please enter your choice");
-    x = x.toLowerCase();
-    return x;
+    console.log(playerScore, compScore);
+
 }
 function round() {
     if (playerChoice == "rock" && compChoice == "scissor") {
         console.log("Player Wins, Rock beats Scissor")
-        ++playerScore;
-    }
-    else if (playerChoice == "paper" && compChoice == "scissor") {
-        console.log("Computer Wins, Scissor beats paper");
-        ++compScore;
-    }
-    else if (playerChoice == "rock" && compChoice == "paper") {
-        console.log("Computer Wins, Paper beats Rock");
-        ++compScore;
+        playerScore++;
+        return;
     }
     else if (playerChoice == "paper" && compChoice == "rock") {
         console.log("Player Wins, Paper beats Rock");
-        ++playerScore;
-    }
-    else if (playerChoice == "scissor" && compChoice == "rock") {
-        console.log("Computer Wins, Rock beats Scissor");
-        ++compScore;
+        playerScore++;
+        return;
     }
     else if (playerChoice == "scissor" && compChoice == "paper") {
         console.log("Player Wins, Scissor beats Paper");
-        ++playerScore;
+        playerScore++;
+        return;
+    }
+    else if (playerChoice == "paper" && compChoice == "scissor") {
+        console.log("Computer Wins, Scissor beats paper");
+        compScore++;
+        return;
+    }
+    else if (playerChoice == "rock" && compChoice == "paper") {
+        console.log("Computer Wins, Paper beats Rock");
+        compScore++;
+        return;
+    }
+    else if (playerChoice == "scissor" && compChoice == "rock") {
+        console.log("Computer Wins, Rock beats Scissor");
+        compScore++;
+        return;
     }
     else if (playerChoice == compChoice) {
         console.log("Draw");
+        return;
     }
+    else {
+        alert("Invaild choice, Enter Rock Paper or Scissor.");
+        return;
+    }
+
 }
 
 
-
-let score = [playerScore, compScore];
-console.log(playerChoice, compChoice)
-console.log(playerScore, compScore); 
